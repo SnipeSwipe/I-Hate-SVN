7cb0daa03e8d6f1a07cfcdc7d5d2b48f2c9bd689
aditimithal <aditi.mithal@gmail.com>
2014-11-15 20:01:26 +0530

    resize field

---
+++ Foosball/src/table/PlayPanel.java
@@ -0,0 +1,12 @@
+package table;
+
+import javax.swing.JPanel;
+
+public class PlayPanel extends JPanel{
+	
+	// private JButton playButton = new JButton("Hellloooo");
+	   // private Table mainFrame;
+    
+	    public PlayPanel(){}
+	    	
+}

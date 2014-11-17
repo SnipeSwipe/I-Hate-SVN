19aa704494f55a8d9f907ea642b8767f3cbd98c1
mallika2608 <mallikaagg@gmail.com>
2014-11-18 01:52:27 +0530

    Added a Formation Class

---
+++ Foosball/src/table/Formation.java
@@ -0,0 +1,15 @@
+package table;
+
+public class Formation 
+{
+	int noOfDefenders;
+	int noOfMidfielders;
+	int noOfAttackers;
+	
+	public Formation(int noOfDefenders, int noOfMidfielders, int noOfAttackers)
+	{
+		this.noOfDefenders = noOfDefenders;
+		this.noOfMidfielders = noOfMidfielders;
+		this.noOfAttackers = noOfAttackers;
+	}
+}

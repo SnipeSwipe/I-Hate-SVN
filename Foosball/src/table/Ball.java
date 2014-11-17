9cee093a95c33b5d4e6fa7da6d1c24168430de8f
aditimithal <aditi.mithal@gmail.com>
2014-11-18 02:07:58 +0530

    ball hits goal n resets

--- Foosball/src/table/Ball.java
+++ Foosball/src/table/Ball.java
@@ -1,9 +1,19 @@
 package table;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Random;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Ball extends Thread{
 	private Ellipse2D.Double ball;
@@ -12,14 +22,15 @@ public class Ball extends Thread{
 	private int dx, dy;          
 	private Color color;
 	private PlayPanel panel; 
-	
+	JLabel image;
+	int startx=0;
+	int starty=0;
 	public Ball(PlayPanel panel){
 		this.panel = panel;
 		isMoving = true;   
 		size=20;
-		speed=50;
-		int startx=0;
-		int starty=0;
+		speed=10;
+		
 		dx=10;
 		dy=10;
 		if(dx==0 && dy==0)  
@@ -61,8 +72,25 @@ public class Ball extends Thread{
 			if(newy+size>panel.getHeight()||newy<0) 
 				dy=-dy; 
 			else dy=+dy; 
-			
-			ball.setFrame(newx, newy, size, size);
+			if((newx ==0&& ((newy>234)&&(newy<=453)))|| (newx ==panel.getWidth()&& ((newy>234)&&(newy<=453))))
+			{
+				BufferedImage myPicture;
+				try {
+					myPicture = ImageIO.read(new File("resources/goal.png"));
+					JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+					panel.add(picLabel);
+					picLabel.setBounds(350,120,600,350);
+					picLabel.setOpaque(true);
+		         	sleep(4000);
+					panel.remove(picLabel);}
+					catch (IOException | InterruptedException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				ball.setFrame(startx, starty, size, size);
+
+			}
+			else ball.setFrame(newx, newy, size, size);
 			panel.repaint();
 			System.out.println(oldx + " " + oldy);
 

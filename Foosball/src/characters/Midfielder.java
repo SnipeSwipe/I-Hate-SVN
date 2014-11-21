acf7c6469eddba978e37611d291b8a499bc22238
Aman Agarwal <snipeswipe@me.com>
2014-11-21 13:54:45 +0530

    Update

--- Foosball/src/characters/Midfielder.java
+++ Foosball/src/characters/Midfielder.java
@@ -2,7 +2,6 @@ package characters;
 
 import java.awt.Color;
 
-
 import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
@@ -14,8 +13,8 @@ import table.PlayPanel;
 
 public class Midfielder extends Player {
 
-	public Midfielder(PlayPanel panel, int starty, int startx,  int dy, int bound, Color color) 
-	{
+	public Midfielder(PlayPanel panel, int starty, int startx, int dy,
+			int bound, Color color) {
 		super(panel, starty, startx, 4, bound, color);
 	}
 

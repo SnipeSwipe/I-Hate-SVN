d29ec8c06ae9c3063a52c4564bbfb27937182a51
Aman Agarwal <snipeswipe@me.com>
2014-11-21 13:55:20 +0530

    Attacker

---
+++ Foosball/src/characters/Attacker.java
@@ -0,0 +1,20 @@
+package characters;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+import java.util.Random;
+
+import javax.swing.JPanel;
+
+import table.PlayPanel;
+
+public class Attacker extends Player {
+
+	public Attacker(PlayPanel panel, int starty, int startx, int dy, int bound,
+			Color color) {
+		super(panel, starty, startx, 6, bound, color);
+	}
+
+}
